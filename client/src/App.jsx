49d7337767c0c16a8922b09964c109080bd687c2
shafeeq8904 <shafeeq8904@gmail.com>
2024-08-29 18:24:20 +0530

    login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes,Route} from 'react-router-dom'
 import IndexPages from './pages/IndexPages'
 import Login from './pages/Login'
 import Layout from './Layout'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Layout/>}>
           <Route index element={<IndexPages/>}/> 
           <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
       </Route>
       
       
